8935d08177e7db7940ba451f69c8a16b8d77a69b
Mantas <mantas.varnagiris@gmail.com>
2020-02-13 11:47:43 +0200

    Initial commit

---
+++ core/src/commonMain/kotlin/com/koduok/mvi/Mvi.kt
@@ -0,0 +1,66 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE")
+
+package com.koduok.mvi
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.io.core.Closeable
+import kotlin.coroutines.CoroutineContext
+
+abstract class Mvi<INPUT, STATE>(initialState: STATE) : CoroutineScope, Closeable {
+    override val coroutineContext: CoroutineContext by lazy { SupervisorJob() + Dispatchers.Main }
+
+    private val inputsChannel by lazy { Channel<INPUT>(Channel.UNLIMITED) }
+    private val statesChannel by lazy { ConflatedBroadcastChannel(initialState) }
+    private val uniqueJobs by lazy { hashMapOf<Any, Job>() }
+
+    val state: STATE get() = statesChannel.value
+    val states: Flow<STATE> get() = statesChannel.asFlow()
+
+    init {
+        launch {
+            for (input in inputsChannel) {
+                handleInput(input).collect { setState(it) }
+            }
+        }
+    }
+
+    fun input(input: INPUT): Boolean {
+        if (inputsChannel.isClosedForSend) return false
+        inputsChannel.offer(input)
+        return true
+    }
+
+    protected abstract fun handleInput(input: INPUT): Flow<STATE>
+
+    protected fun launchUnique(uniqueJobId: Any, block: suspend () -> Unit): Job {
+        val currentJob = uniqueJobs[uniqueJobId]
+        currentJob?.cancel()
+
+        val job = launch { block() }
+        job.invokeOnCompletion { uniqueJobs.remove(uniqueJobId) }
+        uniqueJobs[uniqueJobId] = job
+        return job
+    }
+
+    protected fun cancelUnique(uniqueJobId: Any) {
+        uniqueJobs.remove(uniqueJobId)?.cancel()
+    }
+
+    private fun setState(state: STATE) {
+        val oldState = this.state
+        if (state != oldState) {
+            statesChannel.offer(state)
+        }
+    }
+
+    override fun close() {
+        inputsChannel.close()
+        statesChannel.close()
+        coroutineContext.cancel()
+    }
+}
